cd815366022fa3d5a8ed07bbbfd2e577932259b3
yuichi192168 <yuichi192168@users.noreply.github.com>
2026-04-27 20:36:35 +0800

    final na ata

--- frontend--/src/vite-env.d.ts
+++ frontend--/src/vite-env.d.ts
@@ -9,6 +9,12 @@ interface ImportMetaEnv {
   readonly VITE_FIREBASE_MESSAGING_SENDER_ID: string;
   readonly VITE_FIREBASE_APP_ID: string;
   readonly VITE_FIREBASE_MEASUREMENT_ID?: string;
+  readonly VITE_USE_FIREBASE_AUTH_EMULATOR?: string;
+  readonly VITE_FIREBASE_AUTH_EMULATOR_HOST?: string;
+  readonly VITE_FIREBASE_AUTH_EMULATOR_PORT?: string;
+  readonly VITE_USE_FIRESTORE_EMULATOR?: string;
+  readonly VITE_FIRESTORE_EMULATOR_HOST?: string;
+  readonly VITE_FIRESTORE_EMULATOR_PORT?: string;
   readonly VITE_APP_NAME: string;
   readonly VITE_APP_ENV: string;
 }
